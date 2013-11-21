2a3e6261c16551620da437ab6a676c3777846169
Lenon Kitchens <lkitchens@alertlogic.com>
2013-11-20 19:57:35 -0800

    Added one more test to the "test suite" for list functions

--- test.c
+++ test.c
@@ -29,16 +29,25 @@ list_cb(void *node, void *data) {
         return 0;
     }
 
-    fprintf(stdout, "Element: %d is valid\n", t->a);
     return 1;
+}
+
+void
+delete_node_cb(void *data) {
+    test_struct_t *t = (test_struct_t *)data;
+
+    //fprintf(stdout, "Destroying data at node: %d\n", t->a);
+    free(t);
 }
 
 int32_t test_list() {
     list_t *the_list = NULL;
     test_struct_t *t = NULL;
 
-    list_create(&the_list, NULL);
+    // Create a new list
+    list_create(&the_list, delete_node_cb);
 
+    // Populate the list with pointers to test structures
     for (int32_t i = 0; i < 32768; i++) {
         if ((t = calloc(1, sizeof(test_struct_t))) == NULL) {
             fprintf(stdout, "Error:  Unable to allocate memory for test structures.\n");
@@ -53,7 +62,11 @@ int32_t test_list() {
         list_append(the_list, t);
     }
 
-    fprintf(stdout, "List Size is: %d\n", list_size(the_list));
+    // Verify the size
+    if (list_size(the_list) != 32768) {
+        fprintf(stdout, "List size is: %d.  Expected 32768\n", list_size(the_list));
+        return -1;
+    }
 
     list_for_each(the_list, list_cb, NULL);
 
@@ -62,10 +75,28 @@ int32_t test_list() {
         if ((t->hdr_magic != HDR_MAGIC) || t->ftr_magic != FTR_MAGIC) {
             fprintf(stdout, "TEST 2: Header magic corrupted in list!\n");
         }
+        free(t);
+
         list_tail(the_list, (void **)&t, 1);
         if ((t->hdr_magic != HDR_MAGIC) || t->ftr_magic != FTR_MAGIC) {
             fprintf(stdout, "TEST 2: Footer magic corrupted in list!\n");
         }
+        free(t);
+    }
+
+    // Build the list back up again before destroying it to test the free callback.
+    for (int32_t i = 0; i < 32768; i++) {
+        if ((t = calloc(1, sizeof(test_struct_t))) == NULL) {
+            fprintf(stdout, "Error:  Unable to allocate memory for test structures.\n");
+            return -1;
+        }
+        t->hdr_magic = HDR_MAGIC;
+        t->ftr_magic = FTR_MAGIC;
+        t->a = i;
+        t->b = 256;
+        t->c = 512;
+        t->d = 1024;
+        list_append(the_list, t);
     }
 
     list_destroy(the_list);
